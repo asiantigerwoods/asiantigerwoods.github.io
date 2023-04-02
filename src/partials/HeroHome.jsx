9a654f31a6d9b820bf881d7f5068b8d8e2d0cfb5
asiantigerwoods <nguyentienryan@gmail.com>
2023-04-01 22:53:41 -0700

    added words at d's house

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -37,22 +37,22 @@ function HeroHome() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">
-              test
+              Revolutionizing Your Fitness Journey
             </h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
-              Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+              Find. Share. Connect.
             </p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-green-200 hover:bg-green-100 w-full mb-4 sm:w-auto sm:mb-0" href="#0">
-                  Start free trial
+                  Sign Up Now
                 </a>
               </div>
-              <div data-aos="fade-up" data-aos-delay="600">
+              {/* <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">
                   Learn more
                 </a>
-              </div>
+              </div> */}
             </div>
           </div>
 
