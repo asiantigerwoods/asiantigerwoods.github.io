67e69119a5dd9ac7ce896cd7de4b3b791168b12f
asiantigerwoods <nguyentienryan@gmail.com>
2023-04-05 22:01:30 -0700

    even more

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -13,8 +13,8 @@ function FeaturesZigzag() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-gray-100 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
-            <h1 className="h2 mb-4">One product, unlimited solutions</h1>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
+            <h1 className="h2 mb-4">A new solution right in your hands</h1>
+            <p className="text-xl text-gray-400">Your centralized solution to achieve your fitness objectives. </p>
           </div>
 
           {/* Items */}
