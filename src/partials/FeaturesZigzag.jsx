470d155110fa0c299d0db5d8c3eb3ac4848f6b1b
Darrick-Oliver <darrickwoliver@gmail.com>
2023-04-08 13:40:24 -0700

    Adding images and fixing wording

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import FeatImage01 from '../images/app screenshot 1.png';
-import FeatImage02 from '../images/features-03-image-02.png';
-import FeatImage03 from '../images/features-03-image-03.png';
+import FeatVideo01 from '../images/Workout.mp4';
+import FeatVideo02 from '../images/Routine.mp4';
+import FeatImage03 from '../images/Editor.svg';
+import FeatImage04 from '../images/Profile.svg';
 
 function FeaturesZigzag() {
   return (
@@ -24,38 +25,40 @@ function FeaturesZigzag() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width="540" height="405" alt="Features 01" />
+              <video loop autoPlay muted>
+                  <source src={FeatVideo01} type="video/mp4" />
+                </video>
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More training. Less thinking</div>
-                  <h3 className="h3 mb-3">Intuitive Swiping</h3>
-                  <p className="text-xl text-gray-400 mb-4">Your routine at the top of your phone with easy controls and a rest timer.</p>
+                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More training. Less thinking.</div>
+                  <h3 className="h3 mb-3">Daily exercises</h3>
+                  <p className="text-xl text-gray-400 mb-4">Your routine at the top of your phone for ease of access and mind.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Swipe left to complete all your sets. </span>
+                      <span>Easily reorder exercises based on availability</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Swipe half-way left to complete 1 set.</span>
+                      <span>Quickly log sets or entire exercises with swipe actions</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Swipe right to log in a failed set.</span>
+                      <span>Immediately start a rest timer between sets</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Swipe half-way right to undo.</span>
+                      <span>Algorithmically generate weight for next set</span>
                     </li>
                   </ul>
                 </div>
@@ -66,32 +69,35 @@ function FeaturesZigzag() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width="540" height="405" alt="Features 02" />
+                {/* <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width="540" height="405" alt="Features 02" /> */}
+                <video loop autoPlay muted>
+                  <source src={FeatVideo02} type="video/mp4" />
+                </video>
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More digestable information. Less confusion</div>
-                  <h3 className="h3 mb-3">Database of workouts</h3>
-                  <p className="text-xl text-gray-400 mb-4">Browse for pre-made routines or custom routines.</p>
+                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More centralized. Less confusing.</div>
+                  <h3 className="h3 mb-3">Database of routines</h3>
+                  <p className="text-xl text-gray-400 mb-4">Browse pre-made or custom templates, without the need for any prior gym experience.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Follow and support the creator of the routine.</span>
+                      <span>Match with a routine that meets your goals</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Real user feedback with like/dislike system.</span>
+                      <span>View real feedback with like and dislike system</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Quick summary detailing frequency, difficulty, and focus of routine.</span>
+                      <span>Readily check the focus and difficulty of the routine</span>
                     </li>
                   </ul>
                 </div>
@@ -107,27 +113,27 @@ function FeaturesZigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More effiency. Less effort</div>
-                  <h3 className="h3 mb-3">Step-by-step template</h3>
-                  <p className="text-xl text-gray-400 mb-4">Create your shareable routine within a matter of minutes.</p>
+                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More effiency. Less effort.</div>
+                  <h3 className="h3 mb-3">Step-by-step builder</h3>
+                  <p className="text-xl text-gray-400 mb-4">Create a shareable routine for friends or clients within a matter of minutes.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Color-coded exercises based on muscle group.</span>
+                      <span>Construct custom templates using a simple UI</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Dynamic routines available through varying reps/weights per set.</span>
+                      <span>Create dynamic routines with weight progression</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Easily implement rest days and name each workout day.</span>
+                      <span>Apply advanced progression through custom formulas</span>
                     </li>
                   </ul>
                 </div>
@@ -138,32 +144,32 @@ function FeaturesZigzag() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage03} width="540" height="405" alt="Features 03" />
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage04} width="540" height="405" alt="Features 04" />
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More social. Less boring</div>
+                  <div className="font-architects-daughter text-xl text-green-200 mb-2">More social. Less lonely.</div>
                   <h3 className="h3 mb-3">User profiles</h3>
-                  <p className="text-xl text-gray-400 mb-4">Find your friends and explore new routines.</p>
+                  <p className="text-xl text-gray-400 mb-4">Find friends and show off your progress.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Control how little or how much you want to share.</span>
+                      <span>Control how little or how much you want to share</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Workout Days will keep track of your schedule.</span>
+                      <span>Easily view others' routines used to reach their goals</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Progress automatically tracks your progression from the day you first download Obliq.</span>
+                      <span>Track your own progress and receive periodic reports</span>
                     </li>
                   </ul>
                 </div>
