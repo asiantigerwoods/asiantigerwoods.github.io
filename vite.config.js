c78986e137be75b1b1ae274151056364b568a954
Darrick-Oliver <darrickwoliver@gmail.com>
2023-09-20 16:23:08 -0700

    Trying to get routes to work

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import postcss from './postcss.config.js'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    "process.env": process.env,
   },
   css: {
     postcss,
   },
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: [
@@ -24,6 +25,6 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
-  } 
-})
+    },
+  },
+});
